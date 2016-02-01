732551cf370cb05813ec15878046890638815b55
Chen, Hung <Hung.Chen@Pearson.com>
2016-02-01 12:32:04 -0500

    tests for NotificationComponent

--- test/NotificationComponent.test.js
+++ test/NotificationComponent.test.js
@@ -2,15 +2,21 @@ let NotificationComponent = require("../src/NotificationComponent");
 let expect = require("expect.js");
 
 describe("Notificationcomponent", () => {
+	let notificationComponent = null;
+	beforeEach(() => {
+		notificationComponent = NotificationComponent.getInstance();
+	})
 
 	it("should initialize as singleton", () => {
-		let notificationComponent = NotificationComponent.getInstance();
 		let notificationComponent2 = NotificationComponent.getInstance();
 		expect(notificationComponent).to.be(notificationComponent2);
 	});
 
 	it("should attach the NotificationDropdown to the element", () => {
-		
+		let element = document.createElement("div");
+		notificationComponent.attachComponent(element);
+
+		expect(element.childNodes.length).to.be.above(0);
 	});	
 
 });
