19c9001b144c9efba8285b7c984c8e1ef7264cda
Chen, Hung <Hung.Chen@Pearson.com>
2016-05-16 18:10:46 -0400

    oops

---
+++ src/js/NotificationHeading.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import CoachmarkApi from './CoachmarkApi';
+import FeedbackApi from './FeedbackApi';
+import NotificationApi from './NotificationApi';
+
+export default class NotificationHeading extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        if (this.props.isList) {
+            return (
+                <div>
+                    <h1 className="notification-title--heading">
+                        Notifications
+                    </h1>
+                </div>
+            );
+        }
+
+        if (this.props.isDetails) {
+            return (
+                <div>
+                    <h1 className="notification-title--heading notification-title--back" onClick={this.props.back}>
+                        %3C Back to Notifications
+                    </h1>
+                </div>
+            );
+        }
+        
+    }
+}
