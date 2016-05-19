f43fd06ff1c32a7fbaa5b07ef2ae8371937c03b2
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-05-19 16:00:43 -0600

    remove few things

--- src/js/NotificationApi.js
+++ src/js/NotificationApi.js
@@ -44,7 +44,6 @@ export default class NotificationApi {
 
 	getNotifications() {
 		const response = new Promise((resolve, reject) => {
-			const emptyPayload = '{\r\n\t\"_links\": {\r\n\t\t\"self\": {\r\n\t\t\t\"href\": \"http://notifications-api.prd-prsn.com/usernotifications/recipientid/ffffffff53a846e1e4b08d9451ce5190/dateSince/2014-10-20T02:04:26.177Z/isread/false\"\r\n\t\t}\r\n\t},\r\n\t\"_embedded\": {\r\n\t\t\"usernotifications\": []\r\n\t}\r\n}';
 			const request = new Request(this.url + '/usernotifications/recipientid/' + this.recipientId, {
 				method: 'GET',
 				mode: 'cors',
@@ -54,7 +53,6 @@ export default class NotificationApi {
 				}
 			});
 			fetch(request).then(function(response) {
-				if (response.statusText === 'No Content') { return JSON.parse(emptyPayload)}
 				return response.json();
 			}).then(function(json) {
 				resolve(parseResponse(json));
