23afe445ebdda53ec7420e50700111dae649f15d
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-05-24 13:28:40 -0600

    tests

--- demo/demo.js
+++ demo/demo.js
@@ -2,14 +2,14 @@
 
 import NotificationComponent from '../main'; // for direct API usage
 
-const pt = '<token>';
+const pt = 'eyJhbGciOiJSUzUxMiIsImtpZCI6ImsxMDY5NDgxOTAifQ.eyJleHAiOjE0NjQxMjczMzgsInN1YiI6ImZmZmZmZmZmNTNkYTNjYjNlNGIwZWFhZGRkNTc2ODc3Iiwic2Vzc2lkIjoiMGFlYjg2NTNhOGQ4NGU4MmIzODJmZDE0NmQ1YjIyNDAiLCJ0eXBlIjoiYXQiLCJpYXQiOjE0NjQxMTY1Mzd9.LTRwis0rXGNCM9e5V08dxTKYa-5zzlRT5OA-y5DAnuJpfJLbeSafJdPOJ59Q5LBVzAejZos6EqOpU-0h780GKc9Za0qbmpC08f9kgAag7fPZ8dyd3sBL4qoJxSiqlR-mkzcN509TTCTmH1rYum94i3BU4FzFdF1izFTYDan5pyY';
 
 const AppHeaderConfig = {
 	// NotificationAPI
-	nfApiUrl: '<notificationUrl>',
+	nfApiUrl: 'https://notifications-api.stg-prsn.com',
 	nfContentTypeHeader: 'application/json',
 	nfPiToken: pt,
-	nfRecipientId: '<userId>',
+	nfRecipientId: 'ffffffff5482258ce4b05a12806d3b14',
 
 	// CoachmarkAPI
 	cmApiUrl: 'http://localhost:8080',
--- test/NotificationDetails.test.js
+++ test/NotificationDetails.test.js
@@ -42,7 +42,63 @@ describe('NotificationDetails', () => {
     //         });
     //     });
 
-        describe('toggleList', function() {
+     let list = [{
+        id: '1',
+        link: 'console-stg.pearson.com:8080/account/manage/account',
+        linkText:'Go to Profile Screen',
+        icon: 'fa fa-cogs fa-2x',
+        message: {
+            title: 'title',
+            body: 'body'
+        }
+    }];
+    
+     let archivedList = [{
+        id: '2',
+        title: 'Introducing Profile Options',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo urna at nisi dictum commodo. Curabitur placerat sapien id finibus facilisis. In non leo ultricies, aliquam massa vel, ornare orci. Curabitur sed diam aliquam, mollis velit sit amet, consectetur tellus. Vivamus accumsan, erat vel convallis sagittis, massa velit ornare est, rutrum luctus nulla felis quis lectus. Sed non accumsan nibh. Phasellus placerat odio euismod dui ultricies luctus. Curabitur quis elit efficitur, aliquam nisl feugiat, convallis erat. Sed dictum sem eu purus faucibus, nec convallis purus varius. Duis pretium commodo urna, vitae ullamcorper odio vestibulum non. In a diam pellentesque dui ornare pellentesque a eu ex. Morbi gravida diam id justo euismod, ac luctus nisl tempor. Integer elementum, dui a sollicitudin sollicitudin, quam ipsum blandit libero, nec bibendum nisl eros at velit. In hac habitasse platea dictumst. Vestibulum et ex id nisl iaculis volutpat eu eget eros. Quisque quis justo quis ipsum malesuada luctus. Mauris lobortis enim vitae sapien varius, at ultrices purus malesuada. Ut sed ante fringilla, vehicula felis ac, pellentesque ipsum. Aenean maximus augue massa, quis sodales nunc congue scelerisque. Phasellus euismod libero id lorem elementum dictum. Morbi gravida elit est. In facilisis, justo et sodales rhoncus, risus mauris ornare nisl, eu gravida est neque ut odio.',
+        link: 'console-stg.pearson.com:8080/account/manage/account',
+        linkText:'Go to Profile Screen',
+        icon: 'fa fa-cogs fa-2x',
+        message: {
+            title: 'title',
+            body: 'body'
+        }
+    }];
+
+    let config = {
+        // NotificationAPI
+        nfApiUrl: 'https://notifications-api.stg-prsn.com',
+        nfContentTypeHeader: 'application/json',
+        nfRecipientId: 'ffffffff560c1a1ee4b04ebf43118c60',
+
+        // CoachmarkAPI
+        cmApiUrl: 'http://localhost:8080',
+        cmContentTypeHeader: 'application/json',
+
+        // FeedbackAPI
+        fbApiUrl: 'http://localhost:8080',
+        fbContentTypeHeader: 'application/json'
+    };
+
+    let container = null;
+
+    // mocked close function...
+    let closeDrawerFunction = function() {
+    };
+    
+    let appendArchiveList = function() {
+    };
+    beforeEach(() => {
+        container = ReactTestUtils.renderIntoDocument( 
+            <NotificationDetails notification={this.state.notificationDetails} closeDrawer={this.props.closeDrawer} apiConfig={config} appendArchiveList={appendArchiveList}/>);
+    });
+
+
+
+   
+
+        describe('archiveItem', function() {
             // let notificationComponent;
             // let functionCalled;
             // beforeEach(function() {
@@ -94,10 +150,7 @@ describe('NotificationDetails', () => {
                 expect(markAsViewedCalled).toBe(true);
             });
 
-            afterEach(function() {
-                NotificationComponent.__ResetDependency__('Drawer');
-            });
-        });
+        //});
     });
 
     
