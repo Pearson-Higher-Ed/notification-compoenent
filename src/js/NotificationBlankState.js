f2399b9dea4689ba6c48dc64777a8ce29b9313ba
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-05-24 15:11:16 -0600

    formating

--- src/js/NotificationBlankState.js
+++ src/js/NotificationBlankState.js
@@ -19,10 +19,9 @@ export default class NotificationBlankState extends React.Component {
             return (
                 <div className="notification-blank-page">
                     <h2>Nothing Yet!</h2>
-                    <br/>
                     <h3>We’ll let you know when<br/>something comes up. Till then,<br/>find previous notifications in your
                     <br/>
-                    <a href="javascript:void(0);" onClick={this.archiveList.bind(this)}> Archive </a>
+                    <a href="javascript:void(0);" onClick={this.archiveList.bind(this)}> Archive. </a>
                     </h3>
                 </div>
             );
@@ -31,7 +30,6 @@ export default class NotificationBlankState extends React.Component {
         return (
             <div className="notification-blank-page">
                 <h2>Nothing Here!</h2>
-                <br/>
                 <h3>This is where you will see your<br/>archived notifications.</h3>
             </div>
         );  
