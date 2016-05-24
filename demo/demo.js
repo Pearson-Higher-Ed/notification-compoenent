1033f881e9e17ed854ba5e9eacceaeb9c35a1488
Chen, Hung <Hung.Chen@Pearson.com>
2016-05-24 12:12:35 -0400

    isRead is actually being used now

--- demo/demo.js
+++ demo/demo.js
@@ -2,14 +2,14 @@
 
 import NotificationComponent from '../main'; // for direct API usage
 
-const pt = '<token>';
+const pt = 'eyJhbGciOiJSUzUxMiIsImtpZCI6ImsxMDY5NDgxOTAifQ.eyJleHAiOjE0NjQxMTQzMjEsInN1YiI6ImZmZmZmZmZmNTNkYTNjYjNlNGIwZWFhZGRkNTc2ODc3Iiwic2Vzc2lkIjoiOGMxMTU1MTc2Njg5NDM5N2I3ODU1NThhYWEyY2E5YTciLCJ0eXBlIjoiYXQiLCJpYXQiOjE0NjQxMDM1MjB9.DOe0g1oGfXTF41Wky3FlOQ7wUpGpH83ZgEDPquvhtxWLKzMmhx25pgfq9lxfZ7eSD5ibt91Mpr9tMz42EeehvQbj1OShDPxNhLMl0q6-c6aj2cxHt3HlTidu7igHkxNkBaF7J27QO1S9NdDl1s2D08LtnXX3UkYWmPFRl06bfbQ';
 
 const AppHeaderConfig = {
 	// NotificationAPI
-	nfApiUrl: '<notificationUrl>',
+	nfApiUrl: 'https://notifications-api.stg-prsn.com',
 	nfContentTypeHeader: 'application/json',
 	nfPiToken: pt,
-	nfRecipientId: '<userId>',
+	nfRecipientId: 'ffffffff560c1a1ee4b04ebf43118c60',
 
 	// CoachmarkAPI
 	cmApiUrl: 'http://localhost:8080',
