00ca75bcb9f20582e07d7daeccb1c6083395ffb1
Chen, Hung <Hung.Chen@Pearson.com>
2016-05-24 11:49:03 -0400

    fixed

--- src/js/NotificationBell.js
+++ src/js/NotificationBell.js
@@ -8,16 +8,26 @@ export default class NotificationBell extends React.Component {
 
 	render() {
 		let bellClassNames = 'notification-bell--count';
+		this.count = this.props.unreadCount;
 		console.log(this.props.newNotifications);
 		if (this.props.newNotifications) {
 			bellClassNames += ' notification-bell--new'
 		}
+
+		if (this.props.unreadCount === 0) {
+			bellClassNames += ' hide';
+		}
+
+		if (this.props.unreadCount > 9) {
+			this.count = '9+'; 
+		}
+		
 		return (
 			<div className="notification-bell">
 				<a href="javascript:void(0)" className="notification-bell--activate" onClick={this.props.toggleList}>
 					<i className="pe-icon--bell-o"></i>
 					<div className={bellClassNames}>
-						{this.props.unreadCount}
+						{this.count}
 					</div>
 				</a>
 			</div>
