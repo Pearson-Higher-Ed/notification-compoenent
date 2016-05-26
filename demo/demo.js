ee12d2762011fa130f24242f0efc5e5e03f10f1a
Winn, Richard M <Richard.Winn@Pearson.com>
2016-05-26 10:33:11 -0600

    update

--- demo/demo.js
+++ demo/demo.js
@@ -3,22 +3,22 @@
 import './demo.scss';
 import NotificationComponent from '../main'; // for direct API usage
 
-const pt = 'eyJhbGciOiJSUzUxMiIsImtpZCI6ImsxMDY5NDgxOTAifQ.eyJleHAiOjE0NjQwNDI1MDcsInN1YiI6ImZmZmZmZmZmNTNkYTNjYjNlNGIwZWFhZGRkNTc2ODc3Iiwic2Vzc2lkIjoiNzAwOWM5MmVhMDNhNDFjMzg2MzVlNTRkMmVmOWQ4OWIiLCJ0eXBlIjoiYXQiLCJpYXQiOjE0NjQwMzE3MDZ9.CHlV5XVjLGd6p8tAOza_eWyTX4tfxV1tH4PfCnMxfATL6AUKXjohprVjqOazae8u5zA1tJL2h6AnmxhUNxLn_28L3bcBuK010VIR8I14DkWII1PeCQDzG8uUUSie8AyyCN-SC8d2YGiFE-kljY4FzC99z2tpWTwHHIk7k7s_aSk';
+const pt = '<token>';
 
 const AppHeaderConfig = {
 	// NotificationAPI
-	nfApiUrl: 'https://notifications-api.stg-prsn.com',
+	nfApiUrl: '<notificationUrl>',
 	nfContentTypeHeader: 'application/json',
 	nfPiToken: pt,
-	nfRecipientId: 'ffffffff560c1a1ee4b04ebf43118c60',
+	nfRecipientId: '<userId>',
 
 	// CoachmarkAPI
-	cmApiUrl: 'https://feedback-api.stg-prsn.com',
+	cmApiUrl: '<coachmarkUrl>',
 	cmContentTypeHeader: 'application/json',
 	cmPiToken: pt,
 
 	// FeedbackAPI
-	fbApiUrl: 'https://feedback-api.stg-prsn.com',
+	fbApiUrl: '<feedbackUrl>',
 	fbContentTypeHeader: 'application/json',
 	fbPiToken: pt
 };
