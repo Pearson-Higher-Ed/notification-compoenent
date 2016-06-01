465807bf5981d8c56b9e7cc2845db43312ef1584
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-06-01 10:46:22 -0600

    css fixes

--- demo/demo.js
+++ demo/demo.js
@@ -2,14 +2,14 @@
 
 import NotificationComponent from '../main'; // for direct API usage
 
-const pt = 'eyJhbGciOiJSUzUxMiIsImtpZCI6ImsxMDY5NDgxOTAifQ.eyJleHAiOjE0NjQzODAwMjksInN1YiI6ImZmZmZmZmZmNTNkYTNjYjNlNGIwZWFhZGRkNTc2ODc3Iiwic2Vzc2lkIjoiYzk3MGRjZDdmN2FjNDhkZWFhOGYyZDYwMDNiMWM1ZDMiLCJ0eXBlIjoiYXQiLCJpYXQiOjE0NjQzNjkyMjh9.Kn-Vwo-MPNv-cuQHnAZTOcSxVRgaUwEuobSXwIu_DIvlJ-Bk_2cXX2VMuT4_iVNSEII0gE05Ry3B-T1NgF1w37gtR6eox5ZzCFI_WP3LYEb62PKeVgcceAnGE296wpt4LicOb71YhBOG-f1esZeV6twuCg6vhye1huVURDGVpW4';
+const pt = 'eyJhbGciOiJSUzUxMiIsImtpZCI6ImsxMDY5NDgxOTAifQ.eyJleHAiOjE0NjQ4MDQ3NjcsInN1YiI6ImZmZmZmZmZmNTNkYTNjYjNlNGIwZWFhZGRkNTc2ODc3Iiwic2Vzc2lkIjoiNWZmYjMyMjFjYzg1NDE1ZjlkMjRkOTIxYzRkZWM0OTEiLCJ0eXBlIjoiYXQiLCJpYXQiOjE0NjQ3OTM5Njd9.WqjT7XJ8d0Nwh4G6qVFFtaYgGU_396liqM0a4-g830booOmM7EWXbdg11cA96mftQSOnsgiTttqRYHkK5eS88PHUThYBzsVINqyOuWkx2T2YFUiviAyczljDtoeKEuQV17IebNn8H1LDdt8DF2zy86htnuf-yJLhK0saQCWtV_M';
 
 const AppHeaderConfig = {
 	// NotificationAPI
 	nfApiUrl: 'https://notifications-api.stg-prsn.com',
 	nfContentTypeHeader: 'application/json',
 	nfPiToken: pt,
-	nfRecipientId: 'ffffffff5482258ce4b05a12806d3b14',
+	nfRecipientId: 'ffffffff560c1a1ee4b04ebf43118c60',
 
 	// CoachmarkAPI
 	cmApiUrl: 'http://localhost:8080',
--- src/js/NotificationContainer.js
+++ src/js/NotificationContainer.js
@@ -94,9 +94,6 @@ export default class NotificationContainer extends React.Component {
 						<NotificationHeading back={this.showList.bind(this)} isList={!this.state.isArchive && !this.state.displayDetails} 
 						isDetails={this.state.displayDetails} isArchive={this.state.isArchive}/>
 					</div>
-					<div className="notification-archive--back ">
-						<i className={this.state.isArchive && !this.state.displayDetails ? 'pe-icon--chevron-down pointer' : 'pe-icon--times close-dropdown pointer'} onClick={this.state.isArchive && !this.state.displayDetails ? this.updatedNotificationList.bind(this) : this.props.closeDrawer}></i>
-					</div>	
 				</div>
 				<div className={this.state.displayDetails ? 'hide' : ''}>
 					<NotificationList list={this.state.list}  closeDrawer={this.props.closeDrawer} apiConfig={this.props.config} showDetails={this.showDetails.bind(this)}
@@ -111,7 +108,10 @@ export default class NotificationContainer extends React.Component {
 					<h1 className={this.state.isArchive || this.state.displayDetails ? 'hide' : 'notification-title--heading'}>
 						<a href="javascript:void(0);"> Notification Archive </a> 
 					</h1>
-				</div>	
+				</div>
+				<div className="notification-archive--back">
+					<button onClick={this.state.isArchive && !this.state.displayDetails ? this.updatedNotificationList.bind(this) : this.props.closeDrawer}> <i className={this.state.isArchive && !this.state.displayDetails ? 'pe-icon--chevron-down pointer' : 'pe-icon--times close-dropdown pointer'}></i> </button>
+				</div>		
 			</div>
 		);
 	}
