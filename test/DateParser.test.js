14510735dc0888f3f5c47bf97691afebee75a13f
Winn, Richard M <Richard.Winn@Pearson.com>
2016-06-02 10:26:12 -0600

    feat: adding source

--- test/DateParser.test.js
+++ test/DateParser.test.js
@@ -14,10 +14,10 @@ describe('DateParser', () => {
         let oneHourAgo = new Date(now.getTime() - 600000 * 6);
         expect(DateParser.getFormatDateString(oneHourAgo)).toBe('1 hr');
     });
-    
+
     it('should display 1 day before', () => {
         let now = new Date();
         now.setDate(now.getDate()-1);
-        expect(DateParser.getFormatDateString(now)).toMatch(/[a-zA-Z]{3}\s[a-zA-z]{3}\s\d{2}\s\d{4}/);
+        expect(DateParser.getFormatDateString(now)).toMatch(/[a-zA-Z]{3}\s[a-zA-z]{3}\s\d{1,2}\s\d{4}/);
     });
 });
