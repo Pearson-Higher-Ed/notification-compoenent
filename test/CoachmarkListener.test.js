914d1c956560b5efeabb075ff5297516c683c4eb
Winn, Richard M <Richard.Winn@Pearson.com>
2016-06-10 11:01:12 -0600

    Fix tests

--- test/CoachmarkListener.test.js
+++ test/CoachmarkListener.test.js
@@ -19,11 +19,6 @@ describe('CoachmarkListener.launchCoachmark', () => {
 		coachmarkListener.launchCoachmark(notification);
 
 	});
-
-	it('should default student role to N/A if none is provided', () => {
-		expect(coachmarkListener.cmState[123].targetUserRole).toBe('N/A');
-	});
-
 });
 
 describe('CoachmarkListener.launchCoachmarkIfFromNewUrl', () => {
@@ -39,12 +34,10 @@ describe('CoachmarkListener.launchCoachmarkIfFromNewUrl', () => {
 		cmStateObj = {
 			userNotificationId: 222,
 			userId: 333,
-			targetUserRole: 'stud',
 			masterpieceId: masterpieceId,
 			cmIds: ['555'],
 			index: 0,
 			isVisited: {},
-			likeCmSeries: '',
 			areListenersSet: false
 		};
 		cmStateStr = JSON.stringify(cmStateObj);
@@ -72,7 +65,7 @@ describe('CoachmarkListener.launchCoachmarkIfFromNewUrl', () => {
 	});
 });
 
-describe('Back/Next Listener', () => {
+describe('Back and Next Listeners', () => {
 	let coachmarkListener;
 	let masterpieceId = 444;
 	let closeCmCalled, displayCmCalled;
@@ -87,162 +80,31 @@ describe('Back/Next Listener', () => {
 		coachmarkListener.cmState[masterpieceId] = {
 			userNotificationId: 222,
 			userId: 333,
-			targetUserRole: 'stud',
 			masterpieceId: masterpieceId,
 			cmIds: [554, 555, 556],
 			index: 1,
 			isVisited: {},
-			likeCmSeries: '',
 			areListenersSet: false
 		};
 
 		coachmarkListener.closeCoachmark = (element) => { closeCmCalled = true; };
 		coachmarkListener.getDisplayCoachmark = (masterpieceId) => { displayCmCalled = true; };
-		coachmarkListener.setupBackNextListener(masterpieceId);
+		coachmarkListener.setupBackListener(masterpieceId);
+		coachmarkListener.setupNextListener(masterpieceId);
 	});
 
-	it('should close the old CM and opens the next one when next is triggered', () => {
-		triggerEvent('o-cm-backNext-clicked', { type: 'nextButton', id: 555, payload: '' });
+	it('should open the next CM when next is triggered', () => {
+		triggerEvent('o-cm-next-clicked', { id: masterpieceId, payload: '' });
 
-		expect(closeCmCalled).toBe(true);
 		expect(displayCmCalled).toBe(true);
 		expect(coachmarkListener.cmState[masterpieceId].index).toBe(2);
 	});
 
-	it('should close the old CM and open the previous one when back is triggered', () => {
-		triggerEvent('o-cm-backNext-clicked', { type: 'backButton', id: 555, payload: '' });
+	it('should open the previous CM when back is triggered', () => {
+		triggerEvent('o-cm-previous-clicked', { id: masterpieceId, payload: '' });
 
-		expect(closeCmCalled).toBe(true);
 		expect(displayCmCalled).toBe(true);
 		expect(coachmarkListener.cmState[masterpieceId].index).toBe(0);
-	});
-});
-
-describe('like button listener', () => {
-	let masterpieceId = 444;
-	let coachmarkListener;
-
-	beforeEach(() => {
-		coachmarkListener = new CoachmarkListener({});
-		coachmarkListener.cmState = {};
-		coachmarkListener.cmState[masterpieceId] = {
-			userNotificationId: 222,
-			userId: 333,
-			targetUserRole: 'stud',
-			masterpieceId: masterpieceId,
-			cmIds: [554, 555, 556],
-			index: 1,
-			isVisited: {},
-			likeCmSeries: '',
-			areListenersSet: false
-		};
-		coachmarkListener.setupLikeListener(masterpieceId);
-	});
-
-	it('should flag the cmState as liked when liked is clicked', () => {
-		triggerEvent('o-cm-like-clicked', { type: 'like', id: 555, payload: '' });
-		expect(coachmarkListener.cmState[masterpieceId].likeCmSeries).toBe('like');
-	});
-});
-
-describe('submit button listener', () => {
-	let masterpieceId = 444;
-	let coachmarkListener, submitFeedbackHit, markAsReadHit;
-
-	beforeEach(() => {
-		submitFeedbackHit = false;
-		markAsReadHit = false;
-
-		coachmarkListener = new CoachmarkListener({});
-		coachmarkListener.notificationApi = { markAsRead: () => {markAsReadHit = true;} };
-		coachmarkListener.feedbackApi = { submitFeedback: () => {submitFeedbackHit = true;} };
-		coachmarkListener.closeCoachmark = () => {};
-		coachmarkListener.props = () => {};
-		coachmarkListener.props.apiConfig = () => {};
-
-		coachmarkListener.cmState = {};
-		coachmarkListener.cmState[masterpieceId] = {
-			userNotificationId: 222,
-			userId: 333,
-			targetUserRole: 'stud',
-			masterpieceId: masterpieceId,
-			cmIds: [554, 555, 556],
-			index: 1,
-			isVisited: {},
-			likeCmSeries: 'like',
-			areListenersSet: false
-		};
-		coachmarkListener.setupSubmitListener(masterpieceId);
-	});
-
-	it('should submit user feedback', () => {
-		triggerEvent('o-cm-submit-clicked', { type: 'like', id: 555, payload: 'Some feedback' });
-
-		expect(submitFeedbackHit).toBe(true);
-		expect(markAsReadHit).toBe(true);
-
-	});
-});
-
-describe('Cancel Listener', () => {
-	let masterpieceId = 444;
-	let coachmarkListener;
-
-	beforeEach(() => {
-		coachmarkListener = new CoachmarkListener({});
-
-		coachmarkListener.cmState = {};
-		coachmarkListener.cmState[masterpieceId] = {
-			userNotificationId: 222,
-			userId: 333,
-			targetUserRole: 'stud',
-			masterpieceId: masterpieceId,
-			cmIds: [554, 555, 556],
-			index: 1,
-			isVisited: {},
-			likeCmSeries: 'like',
-			areListenersSet: false
-		};
-
-		coachmarkListener.setupCancelListener(masterpieceId);
-	});
-
-	it('should clear the like/dislike flag from the cmState object', () => {
-		triggerEvent('o-cm-cancel-clicked', { type: 'like', id: 555, payload: 'Some feedback' });
-		expect(coachmarkListener.cmState[masterpieceId].likeCmSeries).toBe('');
-	});
-});
-
-describe('Listener Setup', () => {
-	let masterpieceId = 444;
-	let coachmarkListener = new CoachmarkListener({});
-	coachmarkListener.cmState = {};
-	let isBackNextCalled, isLikeCalled, isSubmitCalled, isCancelCalled;
-
-	beforeEach(() => {
-		isBackNextCalled = isLikeCalled = isSubmitCalled = isCancelCalled = false;
-		coachmarkListener.setupBackNextListener = () => {isBackNextCalled = true;};
-		coachmarkListener.setupLikeListener = () => {isLikeCalled = true;};
-		coachmarkListener.setupSubmitListener = () => {isSubmitCalled = true;};
-		coachmarkListener.setupCancelListener = () => {isCancelCalled = true;};
-	});
-
-	it('should call each listener setup if not already set up', () => {
-		coachmarkListener.cmState[masterpieceId] = { areListenersSet: false };
-		coachmarkListener.cmListenerSetup(masterpieceId);
-		expect(isBackNextCalled).toBe(true);
-		expect(isLikeCalled).toBe(true);
-		expect(isSubmitCalled).toBe(true);
-		expect(isCancelCalled).toBe(true);
-	});
-
-	it('should not call each listener setup if already set up', () => {
-		coachmarkListener.cmState[masterpieceId] = { areListenersSet: true };
-		coachmarkListener.cmListenerSetup(masterpieceId);
-		expect(isBackNextCalled).toBe(false);
-		expect(isLikeCalled).toBe(false);
-		expect(isSubmitCalled).toBe(false);
-		expect(isCancelCalled).toBe(false);
 	});
 });
 
@@ -267,12 +129,10 @@ describe('get display coachmark', () => {
 		coachmarkListener.cmState[masterpieceId] = {
 			userNotificationId: 222,
 			userId: 333,
-			targetUserRole: 'stud',
 			masterpieceId: masterpieceId,
 			cmIds: [554, 555, 556],
 			index: 1,
 			isVisited: {},
-			likeCmSeries: 'like',
 			areListenersSet: false
 		};
 		coachmarkListener.getDisplayCoachmark(masterpieceId);
