fd4b28bc406759b0150732875b8f98b188738bc2
Chen, Hung <Hung.Chen@Pearson.com>
2016-06-10 12:50:10 -0400

    some more testing

--- test/main.test.js
+++ test/main.test.js
@@ -6,99 +6,132 @@ import ReactTestUtils from 'react-addons-test-utils';
 // These tests are kind of "hacky"  I'm mocking out a lot of things that should be able to "rewired" but isn't doing it 
 // properly.  So i took a different approach at it.  They work, but i do not suggest testing anything insde of the constructor
 describe('NotificationComponent', () => {
-    
-    let config = {
-        // NotificationAPI
-        nfApiUrl: 'https://notifications-api.stg-prsn.com',
-        nfContentTypeHeader: 'application/json',
-        nfRecipientId: 'ffffffff560c1a1ee4b04ebf43118c60',
+	
+	let config = {
+		// NotificationAPI
+		nfApiUrl: 'https://notifications-api.stg-prsn.com',
+		nfContentTypeHeader: 'application/json',
+		nfRecipientId: 'ffffffff560c1a1ee4b04ebf43118c60',
 
-        // CoachmarkAPI
-        cmApiUrl: 'http://localhost:8080',
-        cmContentTypeHeader: 'application/json',
+		// CoachmarkAPI
+		cmApiUrl: 'http://localhost:8080',
+		cmContentTypeHeader: 'application/json',
 
-        // FeedbackAPI
-        fbApiUrl: 'http://localhost:8080',
-        fbContentTypeHeader: 'application/json'
-    };
+		// FeedbackAPI
+		fbApiUrl: 'http://localhost:8080',
+		fbContentTypeHeader: 'application/json'
+	};
+
+	let notificationComponent;
+	let closeCalled, toggleCalled, bellComponentUpdateCalled, markAsViewedCalled, containerComponentUpdateCalled;
+	beforeEach(function() {
+		closeCalled = false;
+		toggleCalled = false;
+		bellComponentUpdateCalled = false;
+		markAsViewedCalled = false;
+		containerComponentUpdateCalled = false;
+
+		NotificationComponent.__Rewire__('Drawer', function(config) {
+			this.close = function(){ 
+				closeCalled = true;
+			};
+
+			this.toggle = function() {
+				toggleCalled = true;
+			}
+		});
+
+		notificationComponent = new NotificationComponent(config);
+
+		// override for testing purposes
+		notificationComponent.bellComponent = {
+			forceUpdate: function() {
+				bellComponentUpdateCalled = true;
+			}
+		};
+
+		// override for testing purposes
+		notificationComponent.containerComponent = {
+			forceUpdate: function() {
+				containerComponentUpdateCalled = true;
+			}
+		};
+
+		notificationComponent.notApi = {
+			markAsViewed: function() {
+				markAsViewedCalled = true;
+				return Promise.resolve("");
+			}
+		}
+
+		NotificationComponent.__ResetDependency__('Drawer');
+	});
+
+	afterEach(function() {
+		NotificationComponent.__ResetDependency__('Drawer');
+	});
+
+	describe('NotificationComponent', function() {
+		describe('closeDrawer', function() {
+			it('should close drawer', function() {
+				
+				notificationComponent.closeDrawer();
+
+				expect(closeCalled).toBe(true);
+			});
+		});
+
+		describe('toggleList', function() {
+
+			it('should toggle the drawer', function() {
+				
+				notificationComponent.toggleList();
+
+				expect(toggleCalled).toBe(true);
+			});
+
+			it('should toggle the drawer and mark all items as viewed', function() {
+				notificationComponent.newNotifications = true;
+				notificationComponent.notificationList = [{
+					status: 'CREATED',
+					id: 1
+				}];
 
 
-    describe('NotificationComponent', function() {
-        describe('closeDrawer', function() {
-            it('should close drawer', function() {
-                let functionCalled = false;
+				notificationComponent.toggleList();
 
-                NotificationComponent.__Rewire__('Drawer', function(config) {
-                    this.close = function(){ 
-                        functionCalled = true;
-                    }
-                });
+				expect(toggleCalled).toBe(true);
+				expect(bellComponentUpdateCalled).toBe(true);
+				expect(notificationComponent.newNotifications).toBe(false);
+				expect(markAsViewedCalled).toBe(true);
+			});
 
-                let notificationComponent = new NotificationComponent(config);
-                notificationComponent.closeDrawer();
 
-                expect(functionCalled).toBe(true);
-                NotificationComponent.__ResetDependency__('Drawer');
-            });
-        });
+		});
 
-        describe('toggleList', function() {
-            let notificationComponent;
-            let functionCalled;
-            beforeEach(function() {
-                functionCalled = false;
+		describe('messageListener', function() {
+			it('should add new message to list and update components', function() {
+				const message = {
+					title: "some message",
+					payload: {
+						userNotificationId: 1235,
+						data: '{"some":"jsonObject"}'
+					}
+				};
+				notificationComponent.notificationList = [];
+				notificationComponent.unreadCount = 0;
+				notificationComponent.newNotifications = false;
 
-                NotificationComponent.__Rewire__('Drawer', function(config) {
-                    this.toggle = function(){ 
-                        functionCalled = true;
-                    }
-                });
-                notificationComponent = new NotificationComponent(config);
-            });
+				notificationComponent._messageListener(message);
 
-            it('should toggle the drawer', function() {
-                
-                notificationComponent.toggleList();
+				expect(notificationComponent.notificationList.length).toBe(1);
+				expect(notificationComponent.unreadCount).toBe(1);
+				expect(notificationComponent.newNotifications).toBe(true);
+				expect(bellComponentUpdateCalled).toBe(true);
+				expect(containerComponentUpdateCalled).toBe(true);
+			});
+		});
+	});
 
-                expect(functionCalled).toBe(true);
-            });
-
-            it('should toggle the drawer and mark all items as viewed', function() {
-                let forceUpdateCalled = false;
-                notificationComponent.newNotifications = true;
-                notificationComponent.notificationList = [{
-                    status: 'CREATED',
-                    id: 1
-                }];
-                // i'm mocking this out because it was never called.  This doesnt always work, it only works here because
-                // i had made bellComponent be property of notificationComponent.  Otherwise i would have had to setup a Rewire
-                notificationComponent.bellComponent = {
-                    forceUpdate: function() {
-                        forceUpdateCalled = true;
-                    }
-                };
-
-                let markAsViewedCalled = false;
-                notificationComponent.notApi = {
-                    markAsViewed: function() {
-                        markAsViewedCalled = true;
-                        return Promise.resolve("");
-                    }
-                }
-
-                notificationComponent.toggleList();
-
-                expect(functionCalled).toBe(true);
-                expect(forceUpdateCalled).toBe(true);
-                expect(notificationComponent.newNotifications).toBe(false);
-                expect(markAsViewedCalled).toBe(true);
-            });
-
-            afterEach(function() {
-                NotificationComponent.__ResetDependency__('Drawer');
-            });
-        });
-    });
-
-    
+	
 });
