6d7a62eb3fda9a694f5be6971eaa652234f9bbb3
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-06-21 15:33:19 -0600

    formats

--- src/js/NotificationBell.js
+++ src/js/NotificationBell.js
@@ -14,7 +14,7 @@ const NotificationBell = ({unreadCount, toggleList, newNotifications}) => {
 
 	return (
 		<div className="notification-bell">
-			<a href="javascript:void(0);" className="notification-bell--activate" onClick={toggleList}>
+			<a href="javascript:void(0)" className="notification-bell--activate" onClick={toggleList}>
 				<i className="pe-icon--bell"></i>
 				<div aria-label="NotificationBell" className={bellClassNames}>
 					{unreadCount > 9 ? '9+' : unreadCount}
