5c5d988d3534d46eee12aff19587714b249049d4
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-07-28 06:29:41 -0600

    removing absolute positioning

--- src/js/NotificationBlankState.js
+++ src/js/NotificationBlankState.js
@@ -17,28 +17,20 @@ export default class NotificationBlankState extends React.Component {
 		const contentHeight = {
 			height: window.innerHeight - 175
 		}; 
-		{ /* need to add margin top and bottom for the scenario where the zoomed screen should not have the heading and the description overlapped*/ }
-		const marginpercent = (window.innerHeight - 175 < 350 ? 15 : 0) + '%';
-		const headingMarginBottom = {
-			marginBottom: marginpercent
-		};
-		const descriptionMarginTop = {
-			marginTop: marginpercent
-		};
 		
 		if (this.props.isError) {
 			return (
 			<div className="notification-blank-page" style={contentHeight}>
-				<h2 className="notification-blank-page-heading" style={headingMarginBottom}>Oh, that’s not good</h2>
-				<h3 className="notification-blank-page-description" style={descriptionMarginTop}>There seems to be a problem with this feature.  Try refreshing your browser or clearing your cache.</h3>
+				<h2 className="notification-blank-page-heading">Oh, that’s not good</h2>
+				<h3 className="notification-blank-page-description">There seems to be a problem with this feature.  Try refreshing your browser or clearing your cache.</h3>
 			</div>
 			);
 		}
 		if (!this.props.isArchivedTray) {
 			return (
 				<div className="notification-blank-page" style={contentHeight}>
-					<h2 className="notification-blank-page-heading" style={headingMarginBottom}>Nothing yet!</h2>
-					<h3 className="notification-blank-page-description" style={descriptionMarginTop}>We’ll let you know when<br/>something comes up. Till then,<br/>find previous notifications in your
+					<h2 className="notification-blank-page-heading">Nothing yet!</h2>
+					<h3 className="notification-blank-page-description">We’ll let you know when<br/>something comes up. Till then,<br/>find previous notifications in your
 					<br/>
 					<a href="javascript:void(0);" onClick={this.archiveList.bind(this)}> Archive. </a>
 					</h3>
@@ -48,8 +40,8 @@ export default class NotificationBlankState extends React.Component {
 	   
 		return (
 			<div className="notification-blank-page" style={contentHeight}>
-				<h2 className="notification-blank-page-heading" style={headingMarginBottom}>Nothing here!</h2>
-				<h3 className="notification-blank-page-description" style={descriptionMarginTop}>This is where you will see your<br/>archived notifications.</h3>
+				<h2 className="notification-blank-page-heading">Nothing here!</h2>
+				<h3 className="notification-blank-page-description">This is where you will see your<br/>archived notifications.</h3>
 			</div>
 		);  
 	}
