b9072d48484217c3b4fae9a9bc1f6546e260f0b2
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2016-08-15 12:56:52 -0600

    remove dom purify

--- src/js/NotificationNode.js
+++ src/js/NotificationNode.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import DOMPurify from 'dompurify';
 
 const NotificationNode = ({detailsClick, title, summary, source, archivedNotification, time, isRead, trashIconDisable}) => {
 
@@ -18,7 +17,7 @@ const NotificationNode = ({detailsClick, title, summary, source, archivedNotific
 								{title} 
 							</a>
 						</h2>
-						<div className="notification-node--summary-description pe-label pe-label--small" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(summary)}}/>
+						<div className="notification-node--summary-description pe-label pe-label--small" dangerouslySetInnerHTML={{__html: summary}}/>
 					</div>
 					<div className="notification-node--meta">
 						<div className="notification-node--meta-course">{time}{source ? ' \u00b7 ' : ''}{source}</div>
