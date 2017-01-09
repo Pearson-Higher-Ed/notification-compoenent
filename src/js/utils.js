a6fdbb13c17378da72540f416381711d5ac41b78
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2017-01-09 15:55:09 -0700

    tests fail

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,15 +3,12 @@
 module.exports = {
 	hasHTMLTags : function(s) {
 		const doc = new DOMParser().parseFromString(s, 'text/html');
-		const childNodes = [].slice.call(doc.body.childNodes)
+		const childNodes = doc && [].slice.call(doc.body.childNodes);
 		return this.hasElementNodeType(childNodes);
 	},
 
 	hasElementNodeType : function (nodes) {	
-		if(nodes === undefined) {
-			return '';
-		}
-		return nodes.filter((node) => node.nodeType === 1).length > 0;
+		return nodes && nodes.filter((node) => node.nodeType === 1).length > 0;
 	}
 
 }
