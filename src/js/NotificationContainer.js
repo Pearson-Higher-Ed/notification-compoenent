bb27dad75b1db95aea7c5a254829df5dca1fdb94
Gadde, Tanuja <Tanuja.Gadde@pearson.com>
2017-04-03 12:31:46 -0600

    fix scrolling again

--- src/js/NotificationContainer.js
+++ src/js/NotificationContainer.js
@@ -104,10 +104,10 @@ class NotificationContainer extends React.Component {
 	render() {
 		// this is super dumb because product wants things to "snap" to the bottom
 		const contentHeight = {
-			height: window.innerHeight - 175
+			height: window.innerHeight - 185
 		};
 		const positionTop = {
-			top: window.innerHeight - 175
+			top: window.innerHeight - 185
 		};
 		return (
 			<div aria-label="Notifications Menu" role="menuitem" className="notification-container">
